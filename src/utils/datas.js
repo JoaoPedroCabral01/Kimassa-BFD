a589da786f64d4108468526c22ef286e0440badc
João Pedro Cabral Alves <boyjpgames@gmail.com>
2026-01-21 15:18:53 -0300

    Delete src directory

--- src/utils/datas.js
+++
@@ -1,6 +0,0 @@
-export function diasParaVencer(data) {
-  const hoje = new Date();
-  const validade = new Date(data);
-  const diff = validade - hoje;
-  return Math.ceil(diff / (1000 * 60 * 60 * 24));
-}
